225adcf905e1d4f62ca199ae320a73a7607a48c1
buskoYYY <busko.y.y.y@gmail.com>
2024-07-09 10:43:06 +0300

    Built 1-st level

--- Assets/Game/Scripts/Player/ChangeHat.cs
+++ Assets/Game/Scripts/Player/ChangeHat.cs
@@ -4,25 +4,13 @@ public class ChangeHat : MonoBehaviour
 {
     [Header("Elements")]
     [SerializeField] private GameObject[] _hats;
-    [SerializeField] private Transform _transform;
-    [SerializeField] private Transform _playerPos;
     private GameObject currentHat;
 
-    [Header("Settings")]
-    [SerializeField] private Vector3 _hatAdjustment = new Vector3(1, 1, 1);
-
-    private void Update()
-    {
-        if (currentHat != null)
-        {
-            currentHat.transform.position = _playerPos.position + _hatAdjustment;
-        }
-    }
     public void PutHat()
     {
         if (currentHat != null)
         {
-            Destroy(currentHat);
+            currentHat.SetActive(false);
         }
         int selectedSkin = PlayerPrefs.GetInt("SelectedSkin");
         for (int i = 0; i < _hats.Length; i++)
@@ -30,7 +18,7 @@ public class ChangeHat : MonoBehaviour
             if (i == selectedSkin)
             {
                 currentHat = _hats[i];
-                currentHat = Instantiate(currentHat, _transform.position, Quaternion.identity);
+                currentHat.SetActive(true);
             }
         }
     }
