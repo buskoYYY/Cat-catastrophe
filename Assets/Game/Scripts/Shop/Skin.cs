45211a8f318be15b36dd77683d3fecf9c47b10cb
buskoYYY <busko.y.y.y@gmail.com>
2024-07-10 18:19:43 +0300

    Added timer

--- Assets/Game/Scripts/Shop/Skin.cs
+++ Assets/Game/Scripts/Shop/Skin.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [System.Serializable]
@@ -5,4 +6,7 @@ public class Skin
 {
     public Sprite sprite;
     public int cost;
+
 }
+
+
--- Assets/Game/Scripts/UI/UIDisplay.cs
+++ Assets/Game/Scripts/UI/UIDisplay.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -7,16 +8,25 @@ public class UIDisplay : MonoBehaviour
     [Header("Elements")]
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private TextMeshProUGUI _currencyText;
+    [SerializeField] private TextMeshProUGUI _timeText;
     [SerializeField] private ScoreManager _scoreManager;
     [SerializeField] private CurrencySystem _currencySystem;
+    [SerializeField] private Timer _timer;
+    [SerializeField] private ThirdPersonController _thirdPersonController;
     [SerializeField] private GameObject _pawImage;
     [SerializeField] private GameObject _score;
+    [SerializeField] private GameObject _timeEndPanel;
     [SerializeField] private TweenScale _tweenScale;
+
+    [Header("Setiings")]
+    [SerializeField] private float _timeToLoadPanel;
 
     private void Update()
     {
         _scoreText.text = _scoreManager.GetScore().ToString();
         _currencyText.text = _currencySystem.GetCurrency().ToString();
+        _timeText.text = Mathf.Round( _timer.GetTime()).ToString();
+        OpenTimeEndPanel();
 
     }
 
@@ -24,6 +34,21 @@ public class UIDisplay : MonoBehaviour
     {
         iTween.PunchScale(_pawImage, _tweenScale.scale, _tweenScale.scaleTime);
         iTween.PunchScale(_score, _tweenScale.scale, _tweenScale.scaleTime);
+    }
+
+    private void OpenTimeEndPanel()
+    {
+        if(_timer.IsGameEnded == true)
+        {
+            StartCoroutine(LoadTimeEndScene());
+            _thirdPersonController.MoveDisable();
+        }
+    }
+
+    IEnumerator LoadTimeEndScene()
+    {
+        yield return new WaitForSeconds(_timeToLoadPanel);
+        _timeEndPanel.SetActive(true);
     }
 
 
