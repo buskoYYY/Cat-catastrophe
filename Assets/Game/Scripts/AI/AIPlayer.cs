b3badf44f1167bb56e6121bc2e7fccac07e60990
buskoYYY <busko.y.y.y@gmail.com>
2024-09-16 21:21:09 +0300

    Set  up AI enemy

--- Assets/Game/Scripts/AI/AIPlayer.cs
+++ Assets/Game/Scripts/AI/AIPlayer.cs
@@ -1,18 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIPlayer : MonoBehaviour
+public class AItest : MonoBehaviour
 {
     [Header("Elements")]
-    [SerializeField] private  NavMeshAgent _navMeshAgent;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private Animator _animator;
+    [SerializeField] private Level[] _levels;
 
     [Header("Settings")]
-    [SerializeField] private float radius = 10f;
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRadius;
+    [SerializeField] private float _initialTimeToChangeDest;
+    [SerializeField] private int _startAILevel = 1;
+    [SerializeField] private float _radius = 10f;
+    private float _timeToChangeDest;
 
-    public void PlayAiPlayerAnimation() // когда открывается 5-ый уровень
+
+    private void Start()
     {
-        _animator.Play("walk"); 
+        _timeToChangeDest = _initialTimeToChangeDest;
+        _animator.Play("walk");
+    }
+    private void Update()
+    {
+         _timeToChangeDest -= Time.deltaTime;
+        int roundTime = (int)_timeToChangeDest;
+        if (roundTime < 0)
+        {
+            _timeToChangeDest = _initialTimeToChangeDest;
+        }
+        Debug.Log(_timeToChangeDest);
+
+        if ( roundTime == 0 || _navMeshAgent.remainingDistance < 1.5f)
+        {
+            ChangeDest();
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        if (collision.gameObject.tag == "Hittable")
+        {
+            rigidbody.isKinematic = false;
+            ApplyForce(rigidbody, _explosionForce, _explosionRadius);
+        }
+        if (collision.gameObject.tag == "Hitted")
+        {
+            ApplyForce(rigidbody, _explosionForce, _explosionRadius);
+        }
+    }
+    private void ApplyForce(Rigidbody rigidbody, float explosionForce, float explosionRadius)
+    {
+        rigidbody.AddExplosionForce(explosionForce, transform.position + Vector3.down, explosionRadius, .2f);
     }
 
     private Vector3 RandomNavMeshLocation(float radius) // возвращается вектор 3 который будет будет местом движения кота
@@ -30,14 +72,7 @@ public class AIPlayer : MonoBehaviour
 
     public void ChangeDest()
     {
-        _navMeshAgent.destination = RandomNavMeshLocation(radius);
+        _navMeshAgent.destination = RandomNavMeshLocation(_radius);
     }
 
-    private void Update()
-    {
-        if (_navMeshAgent.remainingDistance < 1.5f)
-        {
-            ChangeDest();
-        }
-    }
 }
